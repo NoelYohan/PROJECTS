edbce8304cd113da1c8b76762a14de6bd8826159
Noel Yohannan <noelyohan007@gmail.com>
2025-09-13 11:50:56 +0530

    changes

--- E commerce/event-backend/routes/event.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createEvent, getEvents, bookEvent } = require('../controllers/eventController');
-
-router.post('/create', createEvent);
-router.get('/', getEvents);
-router.post('/book', bookEvent);
-
-module.exports = router;
