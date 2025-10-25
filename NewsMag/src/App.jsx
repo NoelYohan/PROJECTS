cf3757e9a96f8f0baed002b8db0c19eb50fae0cc
Noel Yohannan <122074908+NoelYohan@users.noreply.github.com>
2025-10-25 23:26:33 +0530

    Add files via upload

---
+++ NewsMag/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import Navbar from "./Component/Navbar"
+import NewsBoard from "./Component/NewsBoard"
+
+
+const App = () => {
+  const [category, setCategory] = useState("general") 
+  return (
+    <div>
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+    </div>
+  )
+}
+
+export default App
---
+++ NewsMag/src/Component/NewsItem.jsx
@@ -0,0 +1,16 @@
+import image from '../assets/depositphotos_283219040-stock-photo-news-icon-background.jpg'
+
+const NewsItem = ({title,description,src,url}) => {
+  return (
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-1 py-1" style={{maxWidth:"345px"}}>
+  <img src={src?src:image} style={{height:"200px",width:"337px"}}className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">{title.slice(0,50)}</h5>
+    <p className="card-text">{description?description.slice(0,90):"Click this to find out more about it !!"}</p>
+    <a href={url} className="btn btn-primary">Read More</a>
+  </div>
+</div>
+  )
+}
+
+export default NewsItem
